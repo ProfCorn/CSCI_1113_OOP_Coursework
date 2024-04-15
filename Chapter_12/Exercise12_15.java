2f4be6116c90dbc7ae8cdadef93ce1f7b4d1468f
ProfCorn <158069625+ProfCorn@users.noreply.github.com>
2024-04-15 08:34:41 -0600

    Update Exercise12_15.java

--- Chapter_12/Exercise12_15.java
+++ Chapter_12/Exercise12_15.java
@@ -5,9 +5,9 @@ import java.io.*;
 
 /*
  * Author:  Randall Dexter
- * Date: 
+ * Date: Apr 15
  * 
- * Explanation:
+ * Explanation: Code that creates a file that has 100 random numbers
  * 
 */
 public class Exercise12_15 {
@@ -18,6 +18,7 @@ public class Exercise12_15 {
 		java.io.PrintWriter output = new java.io.PrintWriter(file);
 		Scanner input = new Scanner(file);
 		Random rand = new Random();
+		int[] intArray = new int[100];
 		if(!new java.io.File("Exercise12_15.txt").exists()) {
 			
 			System.out.print("Does not Exist");
@@ -27,13 +28,21 @@ public class Exercise12_15 {
 			for(int i = 0; i<100;i++) {
 				output.print(rand.nextInt(100)+" ");
 			}
-			while(input.hasNextInt())
-				System.out.print(input.next());
-			
-			
+			output.close();
+			int i = 0;
+			while(input.hasNextInt()) {
+				intArray[i] = Integer.parseInt(input.next());
+				i=i+1;
+			}
+			Arrays.sort(intArray);
+			i=0;
+			for(int val : intArray) {
+				System.out.print(i+": "+val+"\n");
+				i=i+1;
+			}
 
 		}
-		
+		input.close();
 	}
 
 }
