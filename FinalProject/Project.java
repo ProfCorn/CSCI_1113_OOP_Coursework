04e27cb0fd4075e1c45473e8a62751d4a312f048
ProfCorn <158069625+ProfCorn@users.noreply.github.com>
2024-05-21 09:58:05 -0600

    Update Project.java

--- FinalProject/Project.java
+++ FinalProject/Project.java
@@ -1,5 +1,3 @@
-package module_1;
-
 import java.util.*;
 
 /*
@@ -18,12 +16,14 @@ public class Project {
 		Attack AttackTemp = new Attack();
 		Shield ShieldTemp = new Shield();
 		Draw DrawTemp = new Draw();
+		enemy Enemy = new enemy();
+		player Player = new player();
 		for (int i = 0; i<=3;i++) {
 			deck.add(AttackTemp);
-			deck.add(ShieldTemp);
-			deck.add(DrawTemp);
-			
+			deck.add(ShieldTemp);	
 		}
+		deck.add(DrawTemp);
+		deck.add(DrawTemp);
 		deck.add(AttackTemp);
 		deck.add(AttackTemp);
 		ArrayList<Card> discard = new ArrayList<Card>();
@@ -35,6 +35,7 @@ public class Project {
 		
 		while(true) {
 			energy = maxEnergy;
+			Player.shield = 0;
 			//if hand is not full then pick up cards till full
 			if (hand.size()<handSize) {
 				for (int i = hand.size(); i <handSize; i++) {
@@ -52,19 +53,104 @@ public class Project {
 					deck.remove(tempI);	
 				}
 			}
+			
 			/*
 			 * add enemy stuff here
 			 */
+			System.out.println("Health: "+Player.health);
+			System.out.println("Shield: "+Player.shield);
 			System.out.println("Gold: "+gold);
 			System.out.println("Energy: "+energy+"/"+maxEnergy);
 			CardUI(hand);
-			System.out.print("What card would you like to use (0 to bring up help screen)");
+			System.out.print("What card would you like to use (0 to bring up help screen): ");
+			//Grabs choice
+			int tempI = input.nextInt();
+			input.nextLine();
 			
+			if (tempI == 0) {
+				System.out.println("This is the help screen, here we will go over the basics\n");
+				System.out.println("Gold: "+gold+" <-- This is your gold, you get gold from killing enemies and you can spend it on upgrades in the shop\n");
+				System.out.println("Energy: "+energy+"/"+maxEnergy+" <-- This is your energy, the amount of energy you have left is on the left and your total energy is on the right");
+				System.out.println("Energy is used to play cards\n");
+				CardUI(hand);
+				System.out.println("This is your hand, here you see what cards you can play");
+				System.out.println("The number on the left is the energy cost while the number on the right is the amount of the action that is applied\n\n");
+				
+				CardUI(AttackTemp);
+				System.out.println("This is the Attack card, play this card to deal 4 damage to the enemy");
+				CardUI(ShieldTemp);
+				System.out.println("This is the Shield card, play this card to Add 3 shield to yourself");
+				System.out.println("Shield decays each turn");
+				CardUI(DrawTemp);
+				System.out.println("This is the draw card, play this card to draw 2 extra cards");
+				
+				System.out.println("(click enter to continue)");
+				String StrTemp = input.nextLine();
+			}
+			else {
+				hand.get(tempI-1).Use(deck, discard, hand, Enemy, Player);
+				discard.add(hand.get(tempI-1));
+				hand.remove(tempI-1);
+			}
+				
 			
-			//int tempI = input.nextInt();
 		}
 	}
+	private static void CardUI(Card card) {
+		// TODO Auto-generated method stub
+		String tempS = "[     ]";
+		
+		System.out.printf(" %5s ","_____");
+		
+		System.out.print("\n");
+		
+		tempS = "["+card.amount+"    ]";
+		if (card instanceof Shield) {
+			tempS = "[1___"+card.amount+"]";
+		}
+		else if (card instanceof Attack) {
+			tempS = "[1_|_"+card.amount+"]";
+		}
+		else if (card instanceof Draw) {
+			tempS = "[1 _ "+card.amount+"]";
+		}
+		System.out.printf("%5s", tempS);
+		
+		System.out.print("\n");
+		
+		if (card instanceof Shield) {
+			tempS = "[ (+) ]";
+		}
+		else if (card instanceof Attack) {
+			tempS = "[  |  ]";
+		}
+		else if (card instanceof Draw) {
+			tempS = "[ [ ] ]";
+		}
+		System.out.printf("%5s", tempS);
+	
+		System.out.print("\n");
+		
+		if (card instanceof Shield) {
+			tempS = "[ (_) ]";
+		}
+		else if (card instanceof Attack) {
+			tempS = "[  |  ]";
+		}
+		else if (card instanceof Draw) {
+			tempS = "[ [_] ]";
+		}
+		System.out.printf("%5s", tempS);
+		
+		System.out.print("\n");
+		
+		System.out.printf("%5s", "[_____]");
+		
+		System.out.println("\n");
+	}	
+	
 	public static void CardUI(ArrayList<Card> hand) {
+		//outputs the graphics for the Cards in hand
 		String tempS = "[     ]";
 		for (int i = 0; i< hand.size(); i++) {
 			System.out.printf(" %5s ","_____");
@@ -73,13 +159,13 @@ public class Project {
 		for (int i = 0; i< hand.size(); i++) {
 			tempS = "["+hand.get(i).amount+"    ]";
 			if (hand.get(i) instanceof Shield) {
-				tempS = "["+hand.get(i).amount+"___ ]";
+				tempS = "[1___"+hand.get(i).amount+"]";
 			}
 			else if (hand.get(i) instanceof Attack) {
-				tempS = "["+hand.get(i).amount+"_|_ ]";
+				tempS = "[1_|_"+hand.get(i).amount+"]";
 			}
 			else if (hand.get(i) instanceof Draw) {
-				tempS = "["+hand.get(i).amount+" _  ]";
+				tempS = "[1 _ "+hand.get(i).amount+"]";
 			}
 			System.out.printf("%5s", tempS);
 		}
@@ -122,12 +208,16 @@ class enemy{
 	int type;
 	
 }
+class player{
+	int health;
+	int shield;
+}
 
 //The abstract card class, just a way to make the hand work better
 abstract class Card{
 	int cost;
 	int amount;
-	abstract void Use();
+	abstract void Use(ArrayList<Card> deck, ArrayList<Card> discard, ArrayList<Card> hand, enemy Enemy, player Player);
 	Card(){
 		cost = 1;
 	}
@@ -140,8 +230,9 @@ class Attack extends Card {
 	public Attack(int dam) {
 		amount = dam;
 	}
-	public void Use() {
-		System.out.print("Dealt "+amount+" Damage");
+	public void Use(ArrayList<Card> deck, ArrayList<Card> discard, ArrayList<Card> hand, enemy Enemy, player Player) {
+		Enemy.health = Enemy.health - amount;
+		System.out.println("Delt "+amount+" Damage\n");
 	}
 }
 //The draw card
@@ -149,8 +240,23 @@ class Draw extends Card{
 	Draw() {
 		amount = 2;
 	}
-	public void Use() {
-		System.out.print("Drew "+amount+" Cards");
+	public void Use(ArrayList<Card> deck, ArrayList<Card> discard, ArrayList<Card> hand, enemy Enemy, player Player) {
+		for (int i = 0; i <2; i++) {
+			//if out of cards in deck shuffle discard into deck
+			if(deck.size()==0) {
+				System.out.println("Shuffled discard into deck");
+				for(Card card : discard) {
+					deck.add(card);
+				}
+				discard.clear();
+			}
+			//pick random card then add it to hand
+			int tempI = (int)(Math.random()*deck.size());
+			hand.add(deck.get(tempI));
+			deck.remove(tempI);	
+		}
+		System.out.println("Drew "+amount+" Cards\n");
+		
 	}
 	
 }
@@ -159,7 +265,8 @@ class Shield extends Card{
 	Shield(){
 		amount = 3;
 	}
-	public void Use() {
-		System.out.print("Added "+amount+" Shield");
+	public void Use(ArrayList<Card> deck, ArrayList<Card> discard, ArrayList<Card> hand, enemy Enemy, player Player) {
+		
+		System.out.println("Added "+amount+" Shield\n");
 	}
 }
